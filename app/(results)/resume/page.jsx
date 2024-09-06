01208168e81ad51a144c389bd7e6b4a3a3db0576
slenderik <slenderik2@gmail.com>
2024-09-06 18:21:14 +0600

    ALL FOR TODAY

---
+++ app/(results)/resume/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export const metadata  = {
+  title: 'Резюме студентов | ReWork',
+};
+
+export default function Home() {
+  // return ();
+}
---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import '@app/styles/globals.css';
+import '@app/styles/reset.css';
+import { inter } from '@ui/fonts';
+
+export const metadata = {
+  title: "ReWorks",
+  keywords: "работа, работа для студентов, работа для студентов",
+  description: `ReWork - портал для трудоустройства студентов. Здесь собраны актульные вакансии работы и подработки Омска. И резюме реальных студентов.`,
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
