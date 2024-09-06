c98bdab956870c86f0f6758194ad59af67f3db72
slenderik <slenderik2@gmail.com>
2024-09-06 18:21:40 +0600

    ALL FOR TODAY

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,24 @@
 import '@app/styles/globals.css';
 import '@app/styles/reset.css';
 import { inter } from '@ui/fonts';
+import { GlobalHeader } from '@ui/global-header';
+import { GlobalFooter } from '@ui/global-footer';
 
 export const metadata = {
   title: "ReWorks",
   keywords: "работа, работа для студентов, работа для студентов",
   description: `ReWork - портал для трудоустройства студентов. Здесь собраны актульные вакансии работы и подработки Омска. И резюме реальных студентов.`,
+  metadataBase: new URL('https://re-works.ru'),
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalHeader />
+        {children}
+        <GlobalFooter />
+      </body>
     </html>
   );
 }
---
+++ app/ui/global-footer.jsx
@@ -0,0 +1,7 @@
+export default function GlobalFooter() {
+  return (
+    <footer className="">
+      <div className=""></div>
+    </footer>
+  );
+}
