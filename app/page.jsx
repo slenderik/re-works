dc3d2d47ccdca59096aedd22896c857e8d164a50
slenderik <slenderik2@gmail.com>
2024-09-18 14:26:43 +0600

    feat: min-page-h class. fix: rename global.css to main.css

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-[calc(100dvh-8rem)]">
+    <main className="flex flex-col items-center justify-center min-page-h">
       <p>Hero page will be later</p>
       <p>Checkout /Resume</p>
     </main>
