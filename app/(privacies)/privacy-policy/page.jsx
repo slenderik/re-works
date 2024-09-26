86c26b8347fafae8b923941be5c0f80cd9698835
slenderik <slenderik2@gmail.com>
2024-09-26 14:47:31 +0600

    feat!: add privacies

---
+++ app/(privacies)/privacy-policy/page.jsx
@@ -0,0 +1,17 @@
+import MainTemplate from "@/ui/templates/main-template";
+import WrapperTemplate from "@/ui/templates/wrapper-template";
+
+export const metadata  = {
+  title: "Политика конфиденциальности",
+};
+
+export default function PrivacyPolicyPage () {
+  return (
+    <WrapperTemplate>
+      <MainTemplate>
+        <h1>Политика конфиденциальности</h1>
+
+      </MainTemplate>
+    </WrapperTemplate>
+);
+  }
